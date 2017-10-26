2caf41fbbb6a40949c549b08784ee6f048c91cd7
KevinSun <sunb@tjnetsec.com>
2017-10-26 10:03:05 +0800

    修改admob的解决方案

--- AdmobAuto/app/src/main/java/com/admob/sunb/admobauto/AdmobActivity.java
+++ AdmobAuto/app/src/main/java/com/admob/sunb/admobauto/AdmobActivity.java
@@ -2,6 +2,7 @@ package com.admob.sunb.admobauto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,58 +17,64 @@ import com.google.android.gms.ads.MobileAds;
 
 public class AdmobActivity extends Activity {
 
-    private AdView mAdView;
+    private RelativeLayout mainView;
+    private AdView adView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initAdView();
+    }
+
+    private void initAdView() {
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        if (adView != null) {
+            AdRequest adRequest = new AdRequest.Builder().build();
+            adView.loadAd(adRequest);
+        }
     }
 
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
         View view = getLayoutInflater().inflate(layoutResID, null);
-        addAdmob(view);
+        mainView = generateMainView(view);
+        super.setContentView(mainView);
+        addAdViewDelay();
     }
 
-    private void addAdmob(View view) {
-
-        RelativeLayout relativeLayout = generateView(view);
-        super.setContentView(relativeLayout);
-    }
-
-    @NonNull
-    private RelativeLayout generateView(View view) {
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
-        mAdView = new AdView(this);
-        mAdView.setAdSize(AdSize.BANNER);
-        mAdView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-        if (mAdView != null) {
-            AdRequest adRequest = new AdRequest.Builder().build();
-            mAdView.loadAd(adRequest);
-        }
-
+    private RelativeLayout generateMainView(View view) {
         RelativeLayout relativeLayout = new RelativeLayout(this);
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         relativeLayout.setLayoutParams(layoutParams);
         relativeLayout.addView(view, layoutParams);
-
-        RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        relativeLayout.addView(mAdView, layoutParams1);
         return relativeLayout;
+    }
+
+    @NonNull
+    private void addAdViewDelay() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                mainView.addView(adView, layoutParams1);
+            }
+        }, 3000);
     }
 
     @Override
     public void setContentView(View view) {
-        addAdmob(view);
+        mainView = generateMainView(view);
+        super.setContentView(mainView);
+        addAdViewDelay();
     }
 
     @Override
     public void setContentView(View view, ViewGroup.LayoutParams params) {
-        addAdmob(view, params);
-    }
-
-    private void addAdmob(View view, ViewGroup.LayoutParams params) {
-        RelativeLayout relativeLayout = generateView(view);
-        super.setContentView(relativeLayout, params);
+        mainView = generateMainView(view);
+        super.setContentView(mainView, params);
+        addAdViewDelay();
     }
 }
